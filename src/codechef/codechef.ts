0c3200c2ce70f0817a2c817dd6cf897c20e7e669
LemonDrop847 <nitinmishra847@gmail.com>
2023-06-01 11:11:32 +0530

    Removed Unused Imports

--- src/codechef/codechef.ts
+++ src/codechef/codechef.ts
@@ -1,6 +1,5 @@
 import express, { Request, Response } from 'express';
 import axios from 'axios';
-import cheerio from 'cheerio';
 
 const router = express.Router();
 const { load } = require('cheerio');
