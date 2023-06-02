d4f41f1edd0257669779ac3bcf5c57b86f631ba3
LemonDrop847 <nitinmishra847@gmail.com>
2023-06-02 09:19:02 +0530

    Rating Color

--- src/codechef/codechef.ts
+++ src/codechef/codechef.ts
@@ -1,24 +1,33 @@
-import express, { Request, Response } from 'express';
-import axios from 'axios';
+import express, { Request, Response } from "express";
+import axios from "axios";
 
 const router = express.Router();
-const { load } = require('cheerio');
+const { load } = require("cheerio");
 
-router.get('/:username', async (req: Request, res: Response) => {
+function getRatingColor(backgroundColor): string {
+  if (backgroundColor) {
+    return backgroundColor.replace("#", "");
+  }
+  return "";
+}
+
+router.get("/:username", async (req: Request, res: Response) => {
   try {
     const { username } = req.params;
     const url = `https://www.codechef.com/users/${username}`;
 
     const response = await axios.get(url);
-
     const $ = load(response.data);
+    const ratingSpan = $(".rating-star span");
+    const backgroundColor = ratingSpan.css("background-color");
 
-    const rating = $('.rating-number').text().substring(0, 4);
+    const rating = $(".rating-number").text().substring(0, 4);
+    const color = getRatingColor(backgroundColor);
 
-    res.json({ rating });
+    res.json({ rating, color });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Failed to retrieve Codechef rating' });
+    res.status(500).json({ error: "Failed to retrieve Codechef rating" });
   }
 });
 
