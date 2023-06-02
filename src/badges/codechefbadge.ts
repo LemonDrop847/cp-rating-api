f4100d067ae8f1fc15dafc21005449b0b819cfb3
LemonDrop847 <nitinmishra847@gmail.com>
2023-06-02 11:36:05 +0530

    Badge API test

---
+++ src/badges/codechefbadge.ts
@@ -0,0 +1,42 @@
+import express, { Request, Response } from "express";
+import axios from "axios";
+
+const router = express.Router();
+const { load } = require("cheerio");
+
+function getRatingColor(backgroundColor): string {
+  if (backgroundColor) {
+    return backgroundColor.replace("#", "");
+  }
+  return "";
+}
+
+router.get("/:username", async (req: Request, res: Response) => {
+  try {
+    const { username } = req.params;
+    const url = `https://www.codechef.com/users/${username}`;
+
+    const response = await axios.get(url);
+    const $ = load(response.data);
+    const ratingSpan = $(".rating-star span");
+    const backgroundColor = ratingSpan.css("background-color");
+
+    const rating = $(".rating-number").text().substring(0, 4);
+    const color = getRatingColor(backgroundColor);
+    const badge = {
+        schemaVersion: 1,
+        label: 'CodeChef',
+        message: rating,
+        color: color,
+        logo: 'codechef',
+        logoColor: 'f5f5dc',
+        labelColor: '7b5e47',
+      };
+    res.json(badge);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Failed to retrieve Codechef rating" });
+  }
+});
+
+export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import codeforcesRouter from './codeforces/codeforces';
 import codechefRouter from './codechef/codechef';
+import codechefBadge from './badges/codechefbadge';
 
 const app = express();
 const PORT = 3000;
 
 app.use('/codeforces', codeforcesRouter);
 app.use('/codechef', codechefRouter);
+app.use('/codechef/badge',codechefBadge);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
