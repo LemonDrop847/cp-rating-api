6e38f9f838f859160aac81f32749aab8f6a3ffa1
LemonDrop847 <nitinmishra847@gmail.com>
2023-06-02 09:21:16 +0530

    Used CF API

--- src/codeforces/codeforces.ts
+++ src/codeforces/codeforces.ts
@@ -2,23 +2,42 @@ import express, { Request, Response } from 'express';
 import axios from 'axios';
 
 const router = express.Router();
-const { load } = require('cheerio');
+
+const rankColors: { [key: string]: string } = {
+  'legendary grandmaster': 'ff0000',
+  'international grandmaster': 'ff0000',
+  'master': 'ff8c00',
+  'candidate master': '00a0a0',
+  'expert': '0000ff',
+  'specialist': '03a89e',
+  'pupil': '008000',
+  'newbie': '808080'
+};
 
 router.get('/:username', async (req: Request, res: Response) => {
   try {
     const { username } = req.params;
-    const url = `https://codeforces.com/profile/${username}`;
+    const apiUrl = `https://codeforces.com/api/user.info?handles=${username}`;
 
-    const response = await axios.get(url);
+    const response = await axios.get(apiUrl);
+    const userData = response.data;
 
-    const $ = load(response.data);
+    if (userData.status !== 'OK' || !userData.result || userData.result.length === 0) {
+      throw new Error('Failed to retrieve user information');
+    }
 
-    const rating = $('.info ul li span').first().text();
+    const user = userData.result[0];
+    const rating = user.rating.toString();
+    const titlePhoto = user.titlePhoto;
+    const rank = user.rank.toLowerCase();
+    const handle = user.handle;
+    const maxRating = user.maxRating.toString();
+    const color = rankColors[rank] || '';
 
-    res.json({ rating });
+    res.json({ rating, titlePhoto, rank, handle, maxRating, color });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Failed to retrieve Codeforces rating' });
+    res.status(500).json({ error: 'Failed to retrieve user information' });
   }
 });
 
