d371627408026107b0c5c9b64c631a903e9b2dcf
LemonDrop847 <nitinmishra847@gmail.com>
2023-06-03 08:59:21 +0530

    Leetcode API (test)

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import codeforcesRouter from './codeforces/codeforces';
 import codechefRouter from './codechef/codechef';
+import leetcodeRouter from './leetcode/leetcode';
 import codechefBadge from './badges/codechefbadge';
 import codeforcesBadge from './badges/codeforcesbadge';
 
@@ -9,6 +10,7 @@ const PORT = 3000;
 
 app.use('/codeforces', codeforcesRouter);
 app.use('/codechef', codechefRouter);
+app.use('/leetcode',leetcodeRouter)
 app.use('/codechef/badge',codechefBadge);
 app.use('/codeforces/badge',codeforcesBadge);
 
