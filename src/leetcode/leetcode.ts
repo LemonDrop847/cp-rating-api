d1db99866f6c1d1562613c2cf24dda63eda7223d
LemonDrop847 <nitinmishra847@gmail.com>
2023-06-04 07:26:11 +0530

    Leetcode API finalised

--- src/leetcode/leetcode.ts
+++ src/leetcode/leetcode.ts
@@ -10,54 +10,58 @@ router.get("/:username", async (req: Request, res: Response) => {
     const url = 'https://leetcode.com/graphql';
 
     
-    const response = await axios.get(url, {
-        params: {
-          operationName: 'getUserProfile',
-          query: `{
-            matchedUser(username: "${username}") {
-              username
-              languages: languageProblemCount {
-                languageName
-                problemsSolved
-              }
-              submitStats: submitStatsGlobal {
-                acSubmissionNum {
-                  difficulty
-                  count
-                  submissions
-                }
-              }
+    const gqlBody = {
+      query: `query getUserProfile($username: String!) {
+        allQuestionsCount {
+          difficulty
+          count
+        }
+        matchedUser(username: $username) {
+          username
+          profile {
+            reputation
+            ranking
+          }
+          languages: languageProblemCount {
+            languageName
+            problemsSolved
+          }
+          submitStats: submitStatsGlobal {
+            acSubmissionNum {
+              difficulty
+              count
+              submissions
             }
-          }`,
-        },
-      });
-  
-      const data = response.data.data.matchedUser;
-  
-      const languages = data.languages.map((language) => ({
-        languageName: language.languageName,
-        problemsSolved: language.problemsSolved,
-      }));
-  
-      const submitStats = data.submitStats.acSubmissionNum;
-  
-      const problemsSolved = submitStats.reduce((total, stat) => total + stat.count, 0);
-  
-      const totalProblems = problemsSolved + submitStats.reduce((total, stat) => total + stat.submissions - stat.count, 0);
-  
-      const formattedData = {
-        username: data.username,
-        rank: data.rank,
-        problemsSolved,
-        totalProblems,
-        submissions: submitStats.reduce((total, stat) => total + stat.submissions, 0),
-        languages,
-      };
-  
-      res.json(formattedData);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Failed to retrieve LeetCode data' });
-    }
-  });
+          }
+        }
+      }`,
+      variables: { username },
+    };
+
+    const response = await axios.post(url, gqlBody);
+
+    const data = response.data.data.matchedUser;
+
+    const user = data.username;
+    const rank = data.profile.ranking;
+    const problemsSolved = data.submitStats.acSubmissionNum[0].count;
+    const languages = data.languages;
+    const totalProblems = response.data.data.allQuestionsCount[0].count;
+    const submissions = data.submitStats.acSubmissionNum;
+
+    const formattedData = {
+      user,
+      rank,
+      problemsSolved,
+      languages,
+      totalProblems,
+      submissions,
+    };
+
+    res.json(formattedData);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Failed to retrieve LeetCode data' });
+  }
+});
 export default router;
