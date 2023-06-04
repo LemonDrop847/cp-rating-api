2d89969d596e5d21f020023697e7b80eac5ee36a
LemonDrop847 <nitinmishra847@gmail.com>
2023-06-04 07:35:22 +0530

    Leetcode Badge

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import codechefRouter from './codechef/codechef';
 import leetcodeRouter from './leetcode/leetcode';
 import codechefBadge from './badges/codechefbadge';
 import codeforcesBadge from './badges/codeforcesbadge';
+import leetcodeBadge from './badges/leetcodebadge';
 
 const app = express();
 const PORT = 3000;
@@ -13,6 +14,7 @@ app.use('/codechef', codechefRouter);
 app.use('/leetcode',leetcodeRouter)
 app.use('/codechef/badge',codechefBadge);
 app.use('/codeforces/badge',codeforcesBadge);
+app.use('/leetcode/badge',leetcodeBadge)
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
