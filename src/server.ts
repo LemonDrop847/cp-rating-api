062422ad7d6a56a5279a929c1d80e71a4c5385f0
LemonDrop847 <nitinmishra847@gmail.com>
2023-06-06 16:53:29 +0530

    Added home route

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,24 @@
-import express from 'express';
-import codeforcesRouter from './codeforces/codeforces';
-import codechefRouter from './codechef/codechef';
-import leetcodeRouter from './leetcode/leetcode';
-import codechefBadge from './badges/codechefbadge';
-import codeforcesBadge from './badges/codeforcesbadge';
-import leetcodeBadge from './badges/leetcodebadge';
+import express, { Request, Response } from "express";
+import codeforcesRouter from "./codeforces/codeforces";
+import codechefRouter from "./codechef/codechef";
+import leetcodeRouter from "./leetcode/leetcode";
+import codechefBadge from "./badges/codechefbadge";
+import codeforcesBadge from "./badges/codeforcesbadge";
+import leetcodeBadge from "./badges/leetcodebadge";
+import fs from "fs";
 
 const app = express();
 const PORT = 3000;
+app.get("/", (req: Request, res: Response) => {
+  res.sendFile(__dirname + '/static/home.html');
+});
 
-app.use('/codeforces', codeforcesRouter);
-app.use('/codechef', codechefRouter);
-app.use('/leetcode',leetcodeRouter)
-app.use('/codechef/badge',codechefBadge);
-app.use('/codeforces/badge',codeforcesBadge);
-app.use('/leetcode/badge',leetcodeBadge)
+app.use("/codeforces", codeforcesRouter);
+app.use("/codechef", codechefRouter);
+app.use("/leetcode", leetcodeRouter);
+app.use("/codechef/badge", codechefBadge);
+app.use("/codeforces/badge", codeforcesBadge);
+app.use("/leetcode/badge", leetcodeBadge);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
