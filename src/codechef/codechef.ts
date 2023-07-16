21a73b06e62648e7195ced97431e61f65f34826a
LemonDrop847 <nitinmishra847@gmail.com>
2023-07-16 21:05:18 +0530

    Refactoring

--- src/codechef/codechef.ts
+++ src/codechef/codechef.ts
@@ -1,8 +1,8 @@
 import express, { Request, Response } from "express";
 import axios from "axios";
+import cheerio from "cheerio";
 
 const router = express.Router();
-const { load } = require("cheerio");
 
 function calculateStars(rating: number): number {
   if (rating < 1400) {
@@ -28,18 +28,17 @@ router.get("/:username", async (req: Request, res: Response) => {
     const url = `https://www.codechef.com/users/${username}`;
 
     const response = await axios.get(url);
-    const $ = load(response.data);
-    const ratingSpan = $(".rating-star span");
-    const image = $("img.profileImage");
-    const avatar = image.attr("src");
-    const backgroundColor = ratingSpan.css("background-color");
-    const ratingRanksSection = $(".rating-ranks");
-    const country = $(".user-country-name").text();
-    const rankItems = ratingRanksSection.find("li");
-    let countryRank = null;
-    let globalRank = null;
+    const $ = cheerio.load(response.data);
 
-    rankItems.each((index, element) => {
+    const ratingSpan = $(".rating-star span");
+    const avatar = $("img.profileImage").attr("src");
+    const backgroundColor = ratingSpan.css("background-color");
+    const country = $(".user-country-name").text();
+
+    let countryRank: number | null = null;
+    let globalRank: number | null = null;
+
+    $(".rating-ranks li").each((index, element) => {
       const rankText = $(element).text().trim();
       if (rankText.includes("Country Rank")) {
         countryRank = parseInt($(element).find("strong").text());
@@ -47,58 +46,65 @@ router.get("/:username", async (req: Request, res: Response) => {
         globalRank = parseInt($(element).find("strong").text());
       }
     });
+
     const rating = $(".rating-number").text().substring(0, 4);
-    const contestct = parseInt($(".contest-participated-count b").text());
-    const problemsSolvedSection = $(".rating-data-section.problems-solved");
+    const contestCount = parseInt($(".contest-participated-count b").text());
+
     const fullySolvedCount = parseInt(
-      problemsSolvedSection
+      $(".rating-data-section.problems-solved")
         .find('h5:contains("Fully Solved")')
         .text()
         .match(/\((\d+)\)/)[1]
     );
+
     const partialSolvedCount = parseInt(
-      problemsSolvedSection
+      $(".rating-data-section.problems-solved")
         .find('h5:contains("Partially Solved")')
         .text()
         .match(/\((\d+)\)/)[1]
     );
-    const contests = [];
-    problemsSolvedSection.find("strong").each((index, element) => {
+
+    const contests: string[] = [];
+    $(".rating-data-section.problems-solved strong").each((index, element) => {
       const contest = $(element).text().replace(":", "");
       contests.push(contest);
     });
-    const gamesRatingBoxes = $(".games-rating-boxes");
+
     const puzzleRating = parseInt(
-      gamesRatingBoxes
+      $(".games-rating-boxes")
         .find('.games-rating-box-head-text:contains("Puzzle Rating")')
         .closest(".games-rating-box")
         .find(".games-rating-box-body-text")
         .text()
     );
-    const oneVsRating = parseInt(
-      gamesRatingBoxes
+
+    const oneVsOneRating = parseInt(
+      $(".games-rating-boxes")
         .find('.games-rating-box-head-text:contains("1v1 Rating")')
         .closest(".games-rating-box")
         .find(".games-rating-box-body-text")
         .text()
     );
+
     const stars = calculateStars(parseFloat(rating));
+
     const info = {
-      username: username,
-      avatar: avatar,
-      rating: rating,
-      stars: stars,
-      country: country,
-      globalRank: globalRank,
-      countryRank: countryRank,
-      puzzleRating: puzzleRating,
-      onevsoneRating: oneVsRating,
-      participation: contestct,
+      username,
+      avatar,
+      rating,
+      stars,
+      country,
+      globalRank,
+      countryRank,
+      puzzleRating,
+      oneVsOneRating,
+      participation: contestCount,
       color: backgroundColor,
       problemsSolved: fullySolvedCount,
       partialProblems: partialSolvedCount,
-      contests: contests,
+      contests,
     };
+
     res.json(info);
   } catch (error) {
     console.error(error);
